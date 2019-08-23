d8bf3d6798cbfd3da1491c56927dcd3dd3564e5f
nbstsh <nbstsh@gmail.com>
2019-08-23 19:31:28 +0900

    Mocking custom hooks

--- src/components/increment/increment.component.jsx
+++ src/components/increment/increment.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useIncrement from './increment.hook';
 
 const Increment = props => {
--- src/components/increment/increment.test.js
+++ src/components/increment/increment.test.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Increment from './increment.component';
+import useIncrement from './increment.hook';
+
+jest.mock('./increment.hook.js');
 
 describe('[COMPOENNT] Increment', () => {
-	it('should render with initial value 0', () => {
+	it('should render with value from custom hook', () => {
+		useIncrement.mockImplementation(() => {
+			return {
+				value: '50',
+				increment: () => {}
+			};
+		});
+
 		const wrapper = shallow(<Increment />);
-		expect(wrapper.find('.value').text()).toEqual('0');
+		expect(wrapper.find('.value').text()).toEqual('50');
 	});
 
-	it('increments value when clicking +1', () => {
+	it('shold call counter increment when clicking +1', () => {
+		const incrementMock = jest.fn();
+		useIncrement.mockImplementation(() => {
+			return {
+				value: '50',
+				increment: incrementMock
+			};
+		});
 		const wrapper = shallow(<Increment />);
 		wrapper.find('.increment-btn').simulate('click');
-		expect(wrapper.find('.value').text()).toEqual('1');
-	});
-
-	it('should not increment value over 5', () => {
-		const wrapper = shallow(<Increment />);
-		const increment = () => wrapper.find('.increment-btn').simulate('click');
-		[...Array(6)].forEach(() => increment());
-		expect(wrapper.find('.value').text()).toEqual('5');
+		expect(incrementMock).toBeCalled();
 	});
 });
